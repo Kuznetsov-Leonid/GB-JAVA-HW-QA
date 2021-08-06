47136e5cad52a0fa837c384e591f351c0dc090b6
Leonid <orlan1211@yahoo.com>
2021-08-07 00:38:20 +0300

    Revert "Home Work 6"
    This reverts commit 2ea54dc0552fa6de684b02879124f7cca9665f03.

--- src/Lesson6/dog.java
+++ src/Lesson6/dog.java
@@ -1,15 +1,2 @@
-package Lesson6;
-
-public class dog extends animal{
-    private static final String animalType = "dog";
-    public dog(String name, int maxDistanceRun, int maxDistanceSwim, int maxDistanceJump) {
-        super(name, animalType, maxDistanceRun, maxDistanceSwim, maxDistanceJump);
-        ++countDog;
-    }
-
-    public static String getAnimalType() {
-        return animalType;
-    }
-
-    public static int countDog = 0;
+package Lesson6;public class dog {
 }
